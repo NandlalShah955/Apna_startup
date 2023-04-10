98db392e861c5686892961eaa0adb3edb7eb7ac8
Nandlal <shahnandlal49@gmail.com>
2023-04-10 12:29:48 +0530

    working on it

---
+++ startup/src/components/Testimonial.js
@@ -0,0 +1,72 @@
+import React  from 'react'
+import {
+    Box,
+    Flex,
+    Heading,
+    Text,
+    Stack,
+    Container,
+    Avatar,
+    useColorModeValue,
+  } from '@chakra-ui/react';
+
+  const TestimonialContent = ({ children }) => {
+    return (
+      <Stack
+        bg={useColorModeValue('white', 'gray.800')}
+        boxShadow={'lg'}
+        p={8}
+        rounded={'xl'}
+        align={'center'}
+        pos={'relative'}
+        _after={{
+          content: `""`,
+          w: 0,
+          h: 0,
+          borderLeft: 'solid transparent',
+          borderLeftWidth: 16,
+          borderRight: 'solid transparent',
+          borderRightWidth: 16,
+          borderTop: 'solid',
+          borderTopWidth: 16,
+          borderTopColor: useColorModeValue('white', 'gray.800'),
+          pos: 'absolute',
+          bottom: '-16px',
+          left: '50%',
+          transform: 'translateX(-50%)',
+        }}>
+        {children}
+      </Stack>
+    );
+  };
+  const TestimonialHeading = ({ children }) => {
+    return (
+      <Heading as={'h3'} fontSize={'xl'}>
+        {children}
+      </Heading>
+    );
+  };
+  
+  const TestimonialText = ({ children }) => {
+    return (
+      <Text
+        textAlign={'center'}
+        color={useColorModeValue('gray.600', 'gray.400')}
+        fontSize={'sm'}>
+        {children}
+      </Text>
+    );
+  };
+  
+
+
+
+
+
+  function Testimonial() {
+  return (
+    <div>Testimonial</div>
+  )
+}
+
+export default Testimonial
--- startup/src/pages/index.js
+++ startup/src/pages/index.js
@@ -1,9 +1,130 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import { Box, Flex,useDisclosure,Image,Heading,SimpleGrid,IconButton, HStack ,Stack,Button,Text,VStack,useBreakpointValue} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import Plans from "../components/Plans"
 function index() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <div>Hello World</div>
-  )
+    <>
+  <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      w="100%"
+      p={6}
+      bg="teal.500"
+      color="white"
+    >
+      <Flex align="center">
+        {/* <Link to="/"> */}
+          <Box fontWeight="bold" fontSize="xl" letterSpacing="wide">
+            My Website
+          </Box>
+        {/* </Link> */}
+      </Flex>
+
+      <Box display={{ base: "block", md: "none" }} onClick={isOpen ? onClose : onOpen}>
+        {isOpen ? (
+          <CloseIcon w={6} h={6} />
+        ) : (
+          <HamburgerIcon w={6} h={6} />
+        )}
+      </Box>
+
+      <Flex
+        align="center"
+        justify={{ base: "center", md: "flex-end" }}
+        direction={{ base: "column", md: "row" }}
+        pt={{ base: 4, md: 0 }}
+        display={{ base: isOpen ? "flex" : "none", md: "flex" }}
+        width={{ base: "full", md: "auto" }}
+      >
+        {/* <Link to="/about"> */}
+          <Box mt={{ base: 4, md: 0 }} mr={{ base: 0, md: 6 }}>
+            About Us
+          </Box>
+        {/* </Link> */}
+        {/* <Link to="/services"> */}
+          <Box mt={{ base: 4, md: 0 }} mr={{ base: 0, md: 6 }}>
+            Services
+          </Box>
+        {/* </Link> */}
+        {/* <Link to="/contact"> */}
+          <Box mt={{ base: 4, md: 0 }} mr={{ base: 0, md: 6 }}>
+            Contact Us
+          </Box>
+        {/* </Link> */}
+      </Flex>
+    </Flex>
+
+    <Flex
+      w={'full'}
+      h={'100vh'}
+      backgroundImage={
+        'url(https://images.unsplash.com/photo-1600267175161-cfaa711b4a81?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80)'
+      }
+      backgroundSize={'cover'}
+      backgroundPosition={'center center'}>
+      <VStack
+        w={'full'}
+        justify={'center'}
+        px={useBreakpointValue({ base: 4, md: 8 })}
+        bgGradient={'linear(to-r, blackAlpha.600, transparent)'}>
+        <Stack maxW={'2xl'} align={'flex-start'} spacing={6}>
+          <Text
+            color={'white'}
+            fontWeight={700}
+            lineHeight={1.2}
+            fontSize={useBreakpointValue({ base: '3xl', md: '4xl' })}>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit sed do
+            eiusmod tempor
+          </Text>
+          <Stack direction={'row'}>
+            <Button
+              bg={'blue.400'}
+              rounded={'full'}
+              color={'white'}
+              _hover={{ bg: 'blue.500' }}>
+              Show me more
+            </Button>
+            <Button
+              bg={'whiteAlpha.300'}
+              rounded={'full'}
+              color={'white'}
+              _hover={{ bg: 'whiteAlpha.500' }}>
+              Show me more
+            </Button>
+          </Stack>
+        </Stack>
+      </VStack>
+    </Flex>
+
+
+<Heading noOfLines={1} textAlign='center' mt={'20px'} color={'green'}>Services Offered by us</Heading>
+    
+
+    <SimpleGrid  height={"auto"} width={'90%'} margin={'auto'} border='1px solid red' columns={[1,1,2,4]} spacing='40px' mt={'50px'}> 
+  
+    <Box bg='tomato' height='400px'></Box>
+  <Box bg='tomato' height='400px'></Box>
+  <Box bg='tomato' height='400px'></Box>
+  <Box bg='tomato' height='400px'></Box>
+  
+
+
+    </SimpleGrid>
+    
+  
+    <Plans/>
+    
+    
+
+
+
+    </>
+   )
 }
 
-export default index
+export default index;
